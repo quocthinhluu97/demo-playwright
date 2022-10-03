0381871be89f980cd1d65fa2014f3d7b1cd67210
Thinh Luu Quoc <thinh.luu@aswhiteglobal.com>
2022-10-03 10:42:41 +0700

    Save trace file upon failure only

--- playwright.config.ts
+++ playwright.config.ts
@@ -48,7 +48,7 @@ const config: PlaywrightTestConfig = {
     storageState: process.env.STORAGE_STATE_PATH,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
