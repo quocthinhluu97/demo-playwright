7afdb1a51ee89a502afc27024930a23f23a7fcbf
Thinh Luu Quoc <thinh.luu@aswhiteglobal.com>
2022-10-03 12:43:39 +0700

    Refactor test

--- pages/CuraPage.ts
+++ pages/CuraPage.ts
@@ -1,22 +1,59 @@
 import { Locator, Page } from '@playwright/test';
 
+export enum Facilities {
+    TOKYO = "Tokyo CURA Healthcare Center",
+    HONGKONG = "Hongkong CURA Healthcare Center",
+    SEOUL = "Seoul CURA Healthcare Center",
+}
+
+export enum HealthcarePrograms {
+    MEDICARE = "Medicare",
+    MEDICAID = "Medicaid",
+    NONE = "None",
+}
+
 export class CuraPage {
     readonly page: Page;
     readonly makeAppointmentBtn: Locator;
     readonly usernameInput: Locator;
     readonly passwordInput: Locator;
     readonly loginBtn: Locator;
+    readonly bookAppointmentBtn: Locator;
+    facility: string;
+    hospitalReadmission: boolean;
+    healthcareProgram: string;
+    visitDate: Date;
 
     constructor(page: Page) {
         this.page = page;
-        this.makeAppointmentBtn = page.locator('text=Make Appointment');
+        this.makeAppointmentBtn = page.locator('#btn-make-appointment');
         this.usernameInput = page.locator('input[name="username"]');
         this.passwordInput = page.locator('input[name="password"]');
         this.loginBtn = page.locator('button:has-text("Login")');
+        this.bookAppointmentBtn = page.locator('button:has-text("Book Appointment")');
+    }
+
+    async selectFacility(facility: string) {
+        await this.page.locator('select[name="facility"]').selectOption(facility);
+        this.facility = facility;
+    }
+
+    async applyForHospitalReadmission() {
+        await this.page.locator('input[name="hospital_readmission"]').check();
+        this.hospitalReadmission = true;
+    }
+
+    async selectHealthcareProgram(program) {
+        await this.page.locator(`input[type="Radio"][value=${program}]`).check();
+        this.healthcareProgram = program;
     }
 
     async makeAppointment() {
         await this.makeAppointmentBtn.click();
+    }
+
+    async bookAppointment() {
+        await this.bookAppointmentBtn.click();
     }
 
     async login(username, password) {
--- tests/examples/democura.spec.ts
+++ tests/examples/democura.spec.ts
@@ -1,29 +1,46 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
+import { CuraPage, Facilities, HealthcarePrograms } from '@pages/CuraPage';
 
-
-test.beforeEach(async ({ page }, testInfo) => {
-  console.log(`Running ${testInfo.title}`);
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
 });
 
 
-test('test', async ({ page }) => {
-  await page.goto('/');
-  await page.locator('#btn-make-appointment').click();
-  await page.locator('select[name="facility"]').selectOption('Hongkong CURA Healthcare Center');
-  await page.locator('input[name="hospital_readmission"]').check();
-  await page.locator('text=Medicaid >> input[name="programs"]').check();
+test('Book an appointment', async ({ page }) => {
+  const curaPage = new CuraPage(page);
+
+  await curaPage.makeAppointment();
+
+  await curaPage.selectFacility(Facilities.HONGKONG);
+
+  await curaPage.selectFacility(Facilities.TOKYO);
+
+  await curaPage.selectFacility(Facilities.SEOUL);
+
+  await curaPage.applyForHospitalReadmission();
+
+  await curaPage.selectHealthcareProgram(HealthcarePrograms.MEDICAID);
+
+  await curaPage.selectHealthcareProgram(HealthcarePrograms.MEDICARE);
+
+  await curaPage.selectHealthcareProgram(HealthcarePrograms.NONE);
+
   await page.locator('[placeholder="dd\\/mm\\/yyyy"]').click();
-  // Click text=27 >> nth=0
+
   await page.locator('text=27').first().click();
+
   await page.locator('textarea[name="comment"]').click();
+
   await page.locator('textarea[name="comment"]').fill('Hello');
-  await page.locator('text=Book Appointment').click();
+
+  await curaPage.bookAppointment();
 
   await expect(page).toHaveURL('https://katalon-demo-cura.herokuapp.com/appointment.php#summary');
 });
 
 test('Failed test', async ({ page }) => {
-  await page.goto('/');
+  const curaPage = new CuraPage(page);
+
   await page.locator('#btn-make-appointment').click();
 
   await expect(page).toHaveTitle('Dummy');
