e2cd74939e29da2764cf64267efe1a1f04a081e5
thinh <quocthinhluu97@gmail.com>
2023-03-05 10:13:51 +0700

    Add layout header & footer

--- fixtures/base.fixture.ts
+++ fixtures/base.fixture.ts
@@ -1,23 +1,38 @@
 import { test as base } from '@playwright/test';
 import { LoginPage } from "@pages/login.page"
 import { BookAppointmentPage } from '@pages/book-appointment.page';
+import { SummaryPage } from '@pages/summary.page';
+import { HistoryPage } from '@pages/history/history.page';
 
 type BaseFixture = {
     loginPage: LoginPage;
     bookAppointmentPage: BookAppointmentPage;
+    summaryPage: SummaryPage;
 }
 
 export const test = base.extend<BaseFixture>({
     loginPage: async ({ page }, use) => {
-        const pgLogin = new LoginPage(page);
-        await pgLogin.navigate();
-        await use(pgLogin);
+        const loginPage = new LoginPage(page);
+        await loginPage.navigate();
+        await use(loginPage);
     },
 
     bookAppointmentPage: async ({ page }, use) => {
         const bookAppPage = new BookAppointmentPage(page);
         await bookAppPage.navigate();
         await use(bookAppPage);
+    },
+
+    summaryPage: async ({ page }, use) => {
+        const summaryPage = new SummaryPage(page);
+        await summaryPage.navigate();
+        await use(summaryPage);
+    },
+
+    historyPage: async ({ page }, use) => {
+        const historyPage = new HistoryPage(page);
+        await historyPage.navigate();
+        await use(historyPage);
     },
 });
 
---
+++ pages/_layout/navigation-menu.component.ts
@@ -0,0 +1,43 @@
+export class NavigationMenu {
+    readonly page: Page;
+    readonly toggleNavigationMenu: Locator;
+    readonly btnGoToHomePage: Locator;
+    readonly btnGoToLoginPage: Locator;
+    readonly btnGoToHistoryPage: Locator;
+    readonly btnLogout: Locator;
+    readonly navSidebar: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.toggleNavigationMenu = page.locator('#menu-toggle');
+        this.navSidebar = page.locator('#sidebar-wrapper');
+        this.btnGoToHomePage = this.navSidebar.locator('a', { hasText:'Home' });
+        this.btnGoToLoginPage = this.navSidebar.locator('a', { hasText: 'Login' });
+        this.btnGoToHistoryPage = this.navSidebar.locator('a', { hasText: 'History' });
+        this.btnLogout = this.navSidebar.locator('a', { hasText: 'Logout' });
+    }
+
+    async goToHomePage() {
+        await this.toggleNavigationMenu.click();
+        await this.btnGoToHomePage.click();
+        await this.page.waitForLoadState('networkidle');
+    }
+
+    async goToLoginPage() {
+        await this.toggleNavigationMenu.click();
+        await this.btnGoToLoginPage.click();
+        await this.page.waitForLoadState('networkidle');
+    }
+
+    async goToHistoryPage() {
+        await this.toggleNavigationMenu.click();
+        await this.btnGoToHistoryPage.click();
+        await this.page.waitForLoadState('networkidle');
+    }
+
+    async logout() {
+        await this.toggleNavigationMenu.click();
+        await this.btnLogout.click();
+        await this.page.waitForLoadState('networkidle');
+    }
+}
